8627810ecf2dc9270d3cd9b950e14133deff0df9
IMarian <marianilascu@gmail.com>
2014-03-23 21:11:42 +0200

    added entities to project

---
+++ BANProtocolVerfier/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANProtocolVerfier
+{
+    public class Message : IStatement
+    {
+        public List<String> Nonces { get; set; }
+        public List<Key> Keys { get; set; }
+        public List<Message> Messages { get; set; }
+        public String Encrypted { get; set; }
+        public List<Agent> Agents { get; set; }
+
+        public bool Fresh { get; set; }
+    }
+}
---
+++ BANProtocolVerfier/Phase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANProtocolVerfier
+{
+    public class Phase
+    {
+        public Agent Sender { get; set; }
+        public Agent Receiver { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
